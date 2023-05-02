38926156b63750310e96d95e1a4445601c24c2ec
Madhu <madhujagu456@gmail.com>
2023-05-02 10:40:17 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar"
+import './nav.css'
+
+const App =() =>{
+    return(
+        <div>
+             <Navbar></Navbar>
+        </div>
+    )
+
+}
+export default App
